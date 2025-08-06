47475300c2cc6a769020a67f3c1c286a702442b6
Bakhtovarkhuja <shodmonovbakha4@gmail.com>
2025-08-07 00:22:46 +0500

    create if for localStorge to rout to auth

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,14 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+	const router = useRouter()
+
+	if(!localStorage.getItem('access_token')){
+		router.push('/auth')
+	}
+
 	return (
 		<></>
 	)
